abdc5acbd7f118c57b41c51a1fb6c362730b6ba1
Abderahmen jaouadi <abderahmen.jaouadi@esprit.tn>
2025-02-01 12:29:16 +0100

    Configurer le projet Spring Boot et Angular avec Git

---
+++ Interlink_Back/src/main/java/tn/esprit/interlink_back/InterlinkBackApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.interlink_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterlinkBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterlinkBackApplication.class, args);
+    }
+
+}
---
+++ Interlink_Back/src/test/java/tn/esprit/interlink_back/InterlinkBackApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.interlink_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterlinkBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
