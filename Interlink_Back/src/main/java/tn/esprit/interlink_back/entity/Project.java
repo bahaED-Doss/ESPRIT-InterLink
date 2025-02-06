4f9321f269e37aee024ccada819147238e164064
Abderahmen jaouadi <abderahmen.jaouadi@esprit.tn>
2025-02-06 18:05:29 +0100

    added company,project and projectmanager entities and their repos  + Database Config)

---
+++ Interlink_Back/src/main/java/tn/esprit/interlink_back/entity/Project.java
@@ -0,0 +1,27 @@
+package tn.esprit.interlink_back.entity;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long projectId;
+
+    private String title;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company; // Each project belongs to one company
+
+    @ManyToOne
+    @JoinColumn(name = "project_manager_id")
+    private ProjectManager projectManager; // Each project is managed by one manager
+
+}
+
---
+++ Interlink_Back/src/main/java/tn/esprit/interlink_back/entity/ProjectManager.java
@@ -0,0 +1,22 @@
+package tn.esprit.interlink_back.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class ProjectManager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long managerId;
+
+    private String name;
+    private String email;
+    private String password;
+    private boolean status;
+
+    @OneToMany(mappedBy = "projectManager", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Project> projects; // A manager can oversee multiple projects
+
+
+}
