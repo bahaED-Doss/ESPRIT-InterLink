b741824217709045bc8a88f93baf00e1de1d53ef
sameh-bahia <101214740+sameh-bahia@users.noreply.github.com>
2025-02-10 19:35:43 -0800

    1

---
+++ Interlink_Back/src/main/java/tn/esprit/interlink_back/entity/Interview.java
@@ -0,0 +1,17 @@
+package tn.esprit.interlink_back.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public class Interview
+{
+    @Id
+    @GeneratedValue
+    private Long ;
+
+    private String ;
+    private String ;
+
+}
---
+++ Interlink_Back/src/main/java/tn/esprit/interlink_back/entity/Test.java
@@ -0,0 +1,23 @@
+package tn.esprit.interlink_back.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public class Test
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private  int interviewId;
+
+    Application application
+    Date date
+    String type
+
+    private String message;
+    private String givinBy;
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
