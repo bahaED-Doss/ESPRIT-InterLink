4d45abd290a31b0df33438ff275f58dffd12133c
NOURHENNEBELHEDI <100853941+NOURHENNEBELHEDI@users.noreply.github.com>
2025-02-11 23:05:00 +0000

    uodate entity Intership Application
    uodate entity Intership Application

---
+++ Interlink_Back/src/main/java/tn/esprit/interlink_back/entity/Application.java
@@ -0,0 +1,14 @@
+package tn.esprit.interlink_back.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long applicationId;
+    private String status;
+
+
+}
