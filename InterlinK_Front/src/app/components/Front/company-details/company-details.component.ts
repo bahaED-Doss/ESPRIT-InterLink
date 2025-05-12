e87cbe34bd3e4623ad1dd687c19c5eb4f08cfd75
Abderahmen jaouadi <abderahmen.jaouadi@esprit.tn>
2025-05-12 12:58:47 +0100

    ENHANCED FOR THE DEMO

--- InterlinK_Front/src/app/components/Front/company-details/company-details.component.ts
+++ InterlinK_Front/src/app/components/Front/company-details/company-details.component.ts
@@ -31,15 +31,12 @@ export class CompanyDetailsComponent implements OnInit {
       name: ['', [Validators.required, Validators.minLength(3)]], // Name is required with min length 3
       location: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]], // Email validation
-      city: ['', [Validators.required, Validators.pattern('^[a-zA-Z\s]+$')]], // Only allows letters and spaces
-      country: ['', [Validators.required]],
       phone: ['', [Validators.required, Validators.pattern('^[0-9]{8,15}$')]], // Phone number validation
       industrySector: ['', [Validators.required]]
     });
   }
 
   ngOnInit(): void {
-    this.loadCountries();
     const companyId = this.route.snapshot.paramMap.get('id');
     if (companyId) {
       this.loadCompany(Number(companyId));
@@ -49,28 +46,9 @@ export class CompanyDetailsComponent implements OnInit {
   loadCompany(id: number): void {
     this.companyService.getCompanyById(id).subscribe(data => {
       this.companyForm.patchValue(data);
-      this.loadCities(data.country);
     });
   }
 
-  loadCountries(): void {
-    this.http.get<any[]>('https://restcountries.com/v3.1/all').subscribe(data => {
-      this.countries = data.map(country => country.name.common).sort();
-    });
-  }
-
-  onCountryChange(event: any): void {
-    const selectedCountry = event.target.value;
-    this.loadCities(selectedCountry);
-  }
-
-  loadCities(country: string): void {
-    this.http.get<any[]>(`https://api.api-ninjas.com/v1/city?country=${country}`, {
-      headers: { 'X-Api-Key': 'YOUR_API_KEY_HERE' } // Replace with an actual API key
-    }).subscribe(response => {
-      this.cities = response.map(city => city.name);
-    });
-  }
 
   // Save the updated company details
   updateCompany(): void {
